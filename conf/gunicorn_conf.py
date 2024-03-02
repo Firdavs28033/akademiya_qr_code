2797c23b46ca030744c49359b13903e730884353
Firdavs <123739939+Firdavs28033@users.noreply.github.com>
2024-03-02 20:29:43 +0500

    Update gunicorn_conf.py

--- conf/gunicorn_conf.py
+++ conf/gunicorn_conf.py
@@ -1,4 +1,4 @@
-command = '/home/sa_dev/server/new_qr/env/bin/gunicorn'
-pythonpath = '/home/sa_dev/new_qr/config'
+command = '/home/sa_dev/server/loyiha_1/env/bin/gunicorn'
+pythonpath = '/home/sa_dev/loyiha_1/config'
 bind = '172.16.223.227'
 workers = 3
